d64d770e199f193503e8ee21f80f389c6aebebaa
seud <seudsahm1@gmail.com>
2025-09-08 20:58:05 +0300

    feat: npc talk implementd

--- the_forgotten_kingdom/core/npc.py
+++ the_forgotten_kingdom/core/npc.py
@@ -1,10 +1,10 @@
 class NPC:
-    def __init__(self,name,dialogue):
+    def __init__(self,name: str,dialogue: str):
         self.name = name
         self.dialogue = dialogue
         
-    def talk(self,dialogue):
-        pass
+    def talk(self) -> str:
+        return f"{self.name}: {self.dialogue}"
     
     def give_item(self, player,item):
         pass
