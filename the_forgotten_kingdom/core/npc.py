750fbda2179b56531288c1e0eda997199ef527b1
seud <seudsahm1@gmail.com>
2025-09-08 19:44:32 +0300

    feat: add npc class with basic featurees

---
+++ the_forgotten_kingdom/core/npc.py
@@ -0,0 +1,10 @@
+class NPC:
+    def __init__(self,name,dialogue):
+        self.name = name
+        self.dialogue = dialogue
+        
+    def talk(self,dialogue):
+        pass
+    
+    def give_item(self, player,item):
+        pass
