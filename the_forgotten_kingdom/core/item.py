d0bb369a409be3c1e836c4ee8b3eeeb96f0e1f04
seud <seudsahm1@gmail.com>
2025-09-08 19:40:52 +0300

    feat: add item class with basic features

---
+++ the_forgotten_kingdom/core/item.py
@@ -0,0 +1,8 @@
+class Item:
+    def __init__(self,name,effect,value):
+        self.name = name
+        self.effect = effect
+        self.value = value
+        
+    def use(self,target):
+        pass
